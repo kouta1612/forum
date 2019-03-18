dcf12872043915c109e40b458381274fdcd95c00
萩原　孔太 <kota.hagiwara@PC-1950.local>
2019-03-18 14:04:24 +0900

    add profile.show

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,11 +2,15 @@
 
 @section('content')
     <div class="container">
-        <div class="page-heading">
+        <div class="page-header">
             <h1>
                 {{ $profileUser->name }}
                 <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
             </h1>
         </div>
+
+        @foreach ($profileUser->threads as $thread)
+            
+        @endforeach
     </div>
 @endsection
