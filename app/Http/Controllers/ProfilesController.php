30b29c2a7e96add91ea9ed182a1e38aa251c1612
萩原　孔太 <kota.hagiwara@PC-1950.local>
2019-03-18 14:00:37 +0900

    next commit

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ProfilesController extends Controller
 {
     public function show(User $user)
-    {
+    {   
         return view('profiles.show', [
             'profileUser' => $user
         ]);
--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -15,9 +15,21 @@ class ProfilesTest extends TestCase
     {
         $user = create('App\User');
 
-        // dd($user);
+        // dd($user->toArray());
 
-        $this->get("/profiles/{ $user->name }")
+        $this->get("/profiles/{$user->name}")
             ->assertSee($user->name);
+    }
+
+    /** @test */
+    public function profiles_display_all_threads_created_by_associated_user()
+    {
+        $user = create('App\User');
+
+        $thread = create('App\Thread', ['user_id' => $user->id]);
+
+        $this->get("/profiles/{$user->name}")
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
     }
 }
