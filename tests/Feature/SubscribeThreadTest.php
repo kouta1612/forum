afa37a2c7a1f32daa6e7bbe2e2e864509880a7ae
kouta1612 <kouta1612world69@gmail.com>
2019-05-12 13:35:28 +0900

    complete #41

---
+++ tests/Feature/SubscribeThreadTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubscribeThreadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_subscribe_to_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $this->post($thread->path()."/subscriptions");
+
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply here'
+        ]);
+
+        $this->assertCount(1, auth()->user()->notifications);
+    }
+}
