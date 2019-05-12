c5274e61cae4bdde405c96734da3ec4a8f989e65
kouta1612 <kouta1612world69@gmail.com>
2019-05-12 17:13:31 +0900

    complete #42

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -41,7 +41,10 @@
                                 This thread is published {{ $thread->created_at->diffForHumans() }} by
                                 <a href="#">{{ $thread->creator->name }}</a>, and currently
                                 has <span v-text="repliesCount"></span> {{ str_plural('comment', $thread->replies_count) }}.
-                            </p> 
+                            </p>
+                            <p>
+                                <subscribe-button :active="{{ json_encode($thread->isSubscribedTo) }}"></subscribe-button>
+                            </p>
                         </div>
                     </div>
                 </div>
--- tests/Feature/SubscribeThreadTest.php
+++ tests/Feature/SubscribeThreadTest.php
@@ -17,13 +17,27 @@ class SubscribeThreadTest extends TestCase
 
         $thread = create('App\Thread');
 
-        $this->post($thread->path()."/subscriptions");
+        $this->post($thread->path() . "/subscriptions");
 
         $thread->addReply([
             'user_id' => auth()->id(),
             'body' => 'Some reply here'
         ]);
 
-        $this->assertCount(1, auth()->user()->notifications);
+        $this->assertCount(1, $thread->subscriptions);
+    }
+
+    /** @test */
+    public function a_user_can_unsubscribe_from_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $thread->subscribe();
+
+        $this->delete($thread->path() . "/subscriptions");
+
+        $this->assertCount(0, $thread->subscriptions);
     }
 }
