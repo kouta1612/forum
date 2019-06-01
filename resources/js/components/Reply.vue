656c7ed3661da8fd15f02433908872ecb34b51bc
kouta1612 <kouta1612world69@gmail.com>
2019-06-01 15:57:20 +0900

    complete #83

--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -3,13 +3,13 @@
     <div class="card-header" :class="isBest ? ' bg-success' : 'bg-default'">
       <div class="level">
         <h5 class="flex">
-          <a :href="'/profiles/'+data.owner.name" v-text="data.owner.name"></a>
+          <a :href="'/profiles/'+reply.owner.name" v-text="reply.owner.name"></a>
           said
           <span v-text="ago"></span>
         </h5>
 
         <div v-if="$signedIn">
-          <favorite :reply="data"></favorite>
+          <favorite :reply="reply"></favorite>
         </div>
       </div>
     </div>
@@ -28,15 +28,18 @@
       <div v-else v-html="body"></div>
     </div>
 
-    <div class="card-footer level">
-      <div v-if="$authorize('updateReply', reply)">
+    <div
+      class="card-footer level"
+      v-if="$authorize('owns', reply) || $authorize('owns', reply.thread)"
+    >
+      <div v-if="$authorize('owns', reply)">
         <button class="btn btn-xs btn-success mr-1" @click="editting = true">Edit</button>
         <button class="btn btn-xs btn-danger mr-1" @click="destroy">Delete</button>
       </div>
       <button
         class="btn btn-xs btn-secondary ml-a"
         @click="markBestReply"
-        v-show="!isBest"
+        v-if="$authorize('owns', reply.thread)"
       >Best Reply?</button>
     </div>
   </div>
@@ -46,23 +49,22 @@ import Favorite from "./Favorite.vue";
 import moment from "moment";
 
 export default {
-  props: ["data"],
+  props: ["reply"],
 
   components: { Favorite },
 
   data() {
     return {
       editting: false,
-      id: this.data.id,
-      body: this.data.body,
-      isBest: this.data.isBest,
-      reply: this.data
+      id: this.reply.id,
+      body: this.reply.body,
+      isBest: this.reply.isBest
     };
   },
 
   computed: {
     ago() {
-      return moment(this.data.created_at).fromNow() + "...";
+      return moment(this.reply.created_at).fromNow() + "...";
     }
   },
 
@@ -75,7 +77,7 @@ export default {
   methods: {
     update() {
       axios
-        .patch("/replies/" + this.data.id, {
+        .patch("/replies/" + this.id, {
           body: this.body
         })
         .catch(error => {
@@ -88,9 +90,9 @@ export default {
     },
 
     destroy() {
-      axios.delete("/replies/" + this.data.id);
+      axios.delete("/replies/" + this.id);
 
-      this.$emit("deleted", this.data.id);
+      this.$emit("deleted", this.id);
     },
 
     markBestReply() {
