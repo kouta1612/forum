7576cf87a497a14ed9d74cbed7761d5f46eb0f64
kouta1612 <kouta1612world69@gmail.com>
2019-06-02 19:06:10 +0900

    complete #93

--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -2,21 +2,21 @@
 <div class="card" v-if="editing">
     <div class="card-header">
         <div class="level">
-            <input type="text" value="{{ $thread->title }}" class="form-control">
+            <input type="text" class="form-control" v-model="form.title">
         </div>
     </div>
 
     <div class="card-body">
         <div class="form-group">
-            <textarea class="form-control" rows=10>{{ $thread->body }}</textarea>
+            <textarea class="form-control" rows=10 v-model="form.body"></textarea>
         </div>
     </div>
 
     <div class="card-footer">
         <div class="level">
             <button class="btn btn-secondary btn-xs level-item" @click="editing = true" v-show="!editing">Edit</button>
-            <button class="btn btn-primary btn-xs level-item" @click="">Update</button>
-            <button class="btn btn-secondary btn-xs level-item" @click="editing = false">Cansel</button>
+            <button class="btn btn-primary btn-xs level-item" @click="update">Update</button>
+            <button class="btn btn-secondary btn-xs level-item" @click="resetForm">Cancel</button>
 
             @can('update', $thread)
                 <form action="{{ $thread->path() }}" method="POST" class="ml-a">
@@ -36,16 +36,16 @@
             <img src="{{ $thread->creator->avatar_path }}" alt="{{ $thread->creator->name }}" width="25" height="25" class="mr-1">
             <span class="flex">
                 <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                {{ $thread->title }}
+                <span v-text="title"></span>
             </span>
         </div>
     </div>
 
-    <div class="card-body">
-        {{ $thread->body }}
+    <div class="card-body" v-text="body">
+        
     </div>
 
-    <div class="card-footer">
+    <div class="card-footer" v-show="$authorize('owns', thread)">
         <button class="btn btn-secondary btn-xs" @click="editing = true">Edit</button>
     </div>
 </div>
