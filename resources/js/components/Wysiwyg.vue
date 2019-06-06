6eae78737de1011d6fcf760ce5ec80a8b4fd6d59
kouta1612 <kouta1612world69@gmail.com>
2019-06-06 23:53:59 +0900

    fix wysiwyg

--- resources/js/components/Wysiwyg.vue
+++ resources/js/components/Wysiwyg.vue
@@ -1,22 +1,22 @@
 <template>
-    <div>
-        <input type="hidden" id="trix" :name="name">
+  <div>
+    <input type="hidden" id="trix" :name="name">
 
-        <trix-editor input="trix"></trix-editor>
-    </div>
+    <trix-editor ref="trix" input="trix"></trix-editor>
+  </div>
 </template>
 
 <script>
-import Trix from 'trix';
+import Trix from "trix";
 
 export default {
-    props: ['name'],
+  props: ["name"],
 
-    mounted() {
-        this.$refs.trix.addEventListener('trix-change', e => {
-            console.log('Handling');
-            this.$emit('input', e.target.innerHTML);
-        });
-    }
-}
+  mounted() {
+    this.$refs.trix.addEventListener("trix-change", e => {
+      console.log("Handling");
+      this.$emit("input", e.target.innerHTML);
+    });
+  }
+};
 </script>
